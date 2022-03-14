74702d42e9a17d79665604b4f445a6c650ae13dd
anukowsi <kowsalyav375@gmail.com>
2022-03-14 08:35:31 +0530

    kowsi is added

---
+++ src/org/amp/Vaishu.java
@@ -0,0 +1,31 @@
+package org.amp;
+
+public class Vaishu{
+	private void companyName() {
+System.out.println("i am vary happy know");
+int a=10;
+a++;
+float b=20;
+double c=65l;
+c=a+b;
+System.out.println(a+b);
+System.out.println(a);
+--a;
+System.out.println(a);
+System.out.println(c);
+
+	}
+	private void companyId() {
+System.out.println("i am not happy know ");
+	}
+	public static void main(String[] args) {
+		Vaishu v=new Vaishu();
+		v.companyName();
+		v.companyId();
+		
+	}
+		
+	}
+
+
+
